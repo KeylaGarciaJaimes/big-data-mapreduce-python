54fdf5c15030959a2a65631b989f0c4ce85bfc11
KeylaGarciaJaimes <kjgarciaj@unal.edu.co>
2022-06-12 18:17:43 -0500

    Pregunta 10

--- pregunta_10/mapper.py
+++ pregunta_10/mapper.py
@@ -1,3 +1,9 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+import sys
+if __name__ == "__main__":
+
+    for line in sys.stdin:
+        list = line.split("\t")
+        sys.stdout.write("{}\t{}".format(list[0], list[1]))
