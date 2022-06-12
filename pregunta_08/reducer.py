ce2fe78c698fde631e3fc1e3988342938adeef30
KeylaGarciaJaimes <kjgarciaj@unal.edu.co>
2022-06-12 16:50:16 -0500

    Pregunta 08

--- pregunta_08/reducer.py
+++ pregunta_08/reducer.py
@@ -1,3 +1,21 @@
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+import sys
+
+if __name__ == "__main__":
+
+    list = []
+    for line in sys.stdin:
+        line = line.strip("\n")
+        key, val = line.split(",")
+        val = float(val)
+
+        list.append((key, val))
+
+    d = {}
+    for x, y in list:
+        d.setdefault(x, []).append(y)
+
+    for x, y in d.items():
+        sys.stdout.write("{}\t{}\t{}\n".format(x, sum(y), (sum(y)/len(y))))
